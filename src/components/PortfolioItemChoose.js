3fb5b75e1004b5255184798ee786751fee296c8d
jpcdevel <sashakovalenko96@yandex.ru>
2021-09-05 09:50:14 +0300

    fixes

--- src/components/PortfolioItemChoose.js
+++ src/components/PortfolioItemChoose.js
@@ -10,7 +10,7 @@ function PortfolioItemChoose({ portfolio }) {
     return (
         <div
             className={currentPortfolio.id == portfolio.id ? "portfolioItemChoose activeWhite" : "portfolioItemChoose"}
-            onClick={() => history.push(`/portfolio/${portfolio.id}`)}
+            onClick={() => history.push(`portfolio/${portfolio.id}`)}
         >
             <img src={caseImg} alt="" width="25px"/>
             <b className="ms-2">{ portfolio.name }</b>
